4e1777778fec43929a1dc0dfba0cc21c8bbf424a
Bijaygurung <93568290+Bijaygurung@users.noreply.github.com>
2021-11-15 14:08:39 +1100

    Update connection.php

--- LifestyleStore/connection.php
+++ LifestyleStore/connection.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost","id2538044_sajalagrawal","******","id2538044_store") or die(mysqli_error($con));
+$con=mysqli_connect("lifestyledatabase.cniq6yiwxp93.ap-southeast-2.rds.amazonaws.com","admin","Project_2","store") or die(mysqli_error($con));
 //$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
 ?>
